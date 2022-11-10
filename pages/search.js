d2f758012442603d066077665583e287c7c656c4
Smart Oghenemine <35765212+VinciDemons@users.noreply.github.com>
2022-11-10 15:58:07 +0100

    create the about data section and modify the title in search page

--- pages/search.js
+++ pages/search.js
@@ -1,26 +1,40 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
-import Response from "../Response"
+import SearchResults from "../components/SearchResults";
+import Response from "../Response";
+import { useRouter } from "next/router";
 
-export default function search({results}) {
+export default function search({ results }) {
+  const router = useRouter();
   console.log(results);
+
   return (
     <div>
       <Head>
-        <title>Search Page</title>
+        <title>{router.query.term}</title>
       </Head>
 
       <SearchHeader />
+
+      <SearchResults results={results} />
     </div>
   );
 }
 
-export async function getServerSideProps(context){
-  const mockData = true
-  const data = mockData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`).then((response)  => response.json())
-  return  {
-    props:{
-      results: data
-    }
-  }
+export async function getServerSideProps(context) {
+  const mockData = true;
+  const data = mockData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+          context.query.searchType && "&searchType=image"
+        }`
+      ).then((response) => response.json());
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
